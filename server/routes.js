9219b5ae386aad2cdb982bc9c6af38972b38f538
Ryan Nguyen <136216708+ryananhtuan-nguyen@users.noreply.github.com>
2023-08-04 14:55:49 +1200

    M V P

--- server/routes.js
+++ server/routes.js
@@ -20,6 +20,26 @@ quizzz.get('/', async (req, res) => {
 quizzz.post('/', async (req, res) => {
   const filePath = Path.join(__dirname, 'data', 'results.json')
   const data = await fs.readFile(filePath, 'utf-8')
-  const result = JSON.parse(data)
-  res.render('result', result.pokemon[2])
+  const result = JSON.parse(data).pokemon
+
+  //Grab scores array from the form
+  let body = req.body
+  let scores = Object.values(body)
+  let scoreArr = []
+  for (let item of scores) {
+    scoreArr.push(Number(item))
+  }
+
+  let totalScore = scoreArr.reduce((total, current) => total + current)
+  console.log(totalScore)
+  //conditions to render page
+  if (11 < totalScore < 16) {
+    res.render('result', result[2])
+  } else if (15 < totalScore < 20) {
+    res.render('result', result[3])
+  } else if (19 < totalScore < 25) {
+    res.render('result', result[1])
+  } else {
+    res.render('result', result[0])
+  }
 })
